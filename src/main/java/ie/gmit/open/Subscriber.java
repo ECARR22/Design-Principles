815e7958026c5e0ce949e17dd8f9c3ce64ccb9b8
Evan Carr <evancarr97@gmail.com>
2020-03-27 22:10:02 +0000

    .

---
+++ src/main/java/ie/gmit/open/Subscriber.java
@@ -0,0 +1,36 @@
+package ie.gmit.open;
+
+// closed for modification
+public abstract class Subscriber {
+
+    Long subscriberId;
+    private String address;
+    private Long phoneNumber;
+
+    public Long getSubscriberId() {
+        return subscriberId;
+    }
+
+    public void setSubscriberId(Long subscriberId) {
+        this.subscriberId = subscriberId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public abstract double calculateBill();
+
+}
